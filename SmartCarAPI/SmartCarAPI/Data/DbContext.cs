adae8522f17971781d20b9b69902004dab0f8cce
Jorrit Geurts <r0956231@student.thomasmore.be>
2024-10-29 11:56:45 +0100

    API take 3 push

---
+++ SmartCarAPI/SmartCarAPI/Data/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartCarAPI.Models;
+
+namespace SmartCarAPI.Data
+{
+    public class ApplicationDbContext :  DbContext
+    {
+        public ApplicationDbContext () { }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Damage> Damage { get; set; }
+    }
+}
